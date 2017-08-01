f5d91f6d20ce740efa1512593d21e966c0413ecf
王二民 <wangem@lesaas.cn>
2017-08-01 17:40:26 +0800

    增加项目基本结构

---
+++ src/main/java/com/wem/voice/VoiceServerStart.java
@@ -0,0 +1,12 @@
+package com.wem.voice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceServerStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceServerStart.class, args);
+	}
+}
